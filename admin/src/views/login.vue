47bf245714806f6fe9b53e5b8d012f7eeb4ed9c5
zuobiao <zuobiao-x@foxmail.com>
2020-12-19 21:38:11 +0800

    给login页面登录按钮绑定点击事件，跳转到admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -36,7 +36,7 @@
   															<input type="checkbox" class="ace" />
   															<span class="lbl">记住我</span>
   														</label>
-  														<button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+  														<button @click="login" type="button" class="width-35 pull-right btn btn-sm btn-primary">
   															<i class="ace-icon fa fa-key"></i>
   															<span class="bigger-110">登录</span>
   														</button>
@@ -62,6 +62,11 @@
 
 export default {
   name: 'Login',
+	methods: {
+		login(){
+			this.$router.push("/admin");
+		}
+	}
 }
 </script>
 
