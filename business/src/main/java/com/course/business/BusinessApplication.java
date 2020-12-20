6222b372449251a9879285ca69a6911c40a6756d
zuobiao <zuobiao-x@foxmail.com>
2020-12-20 09:58:08 +0800

    增加business模块，并增加初始启动代码

--- business/src/main/java/com/course/business/BusinessApplication.java
+++ business/src/main/java/com/course/business/BusinessApplication.java
@@ -1,4 +1,4 @@
-package com.course.system;
+package com.course.business;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -11,14 +11,13 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 @MapperScan("com.course.server.mapper")
 @EnableEurekaClient
-public class SystemApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SystemApplication.class);
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("Business地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
