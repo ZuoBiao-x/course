deab870fba3651e5157f1c7ebe83ea4fdb29e06b
zuobiao <zuobiao-x@foxmail.com>
2020-12-27 12:21:20 +0800

    大文件断点续传与极速秒传：基于OSS接口实现文件上传，增加文件追加上传oss-append和简单上传oss-simple

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -94,7 +94,8 @@
                 }
 
                 // 文件分片
-                let shardSize = 10 * 1024 * 1024;    //以20MB为一个分片
+                let shardSize = 10 * 1024 * 1024;    //以10MB为一个分片
+                // let shardSize = 50 * 1024;    //以50k为一个分片
                 let shardIndex = 1;		//分片索引，1表示第1个分片
                 let size = file.size;
                 let shardTotal = Math.ceil(size / shardSize); //总片数
@@ -164,7 +165,8 @@
 
                     param.shard = base64;
 
-                    _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', param).then((response) => {
+                    // _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', param).then((response) => {
+                    _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/oss-append', param).then((response) => {
                         let resp = response.data;
                         console.log("上传文件成功：", resp);
                         Progress.show(parseInt(shardIndex * 100 / shardTotal));
