d58949e7d08de28df14d3b8893e08ff127b7d96c
zuobiao <zuobiao-x@foxmail.com>
2020-12-28 17:52:25 +0800

    用户管理与登录：集成redis，图片验证码的存储从session改为redis

--- system/src/main/java/com/course/system/config/BeanConfiguration.java
+++ system/src/main/java/com/course/system/config/BeanConfiguration.java
@@ -1,7 +1,6 @@
 package com.course.system.config;
 
-import com.course.server.config.LogAspect;
-import com.course.server.service.*;
+import com.course.server.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
