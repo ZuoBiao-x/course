dc8613c062d0dec1382575fb9d936423fb912135
zuobiao <zuobiao-x@foxmail.com>
2020-12-28 14:29:04 +0800

    用户管理与登录：基本的登录功能开发，校验用户名密码

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -20,13 +20,13 @@
   												<fieldset>
   													<label class="block clearfix">
   														<span class="block input-icon input-icon-right">
-  															<input type="text" class="form-control" placeholder="Username" />
+  															<input v-model="user.loginName" type="text" class="form-control" placeholder="Username" />
   															<i class="ace-icon fa fa-user"></i>
   														</span>
   													</label>
   													<label class="block clearfix">
   														<span class="block input-icon input-icon-right">
-  															<input type="password" class="form-control" placeholder="Password" />
+  															<input v-model="user.password" type="password" class="form-control" placeholder="Password" />
   															<i class="ace-icon fa fa-lock"></i>
   														</span>
   													</label>
@@ -63,9 +63,26 @@
             $('body').removeClass('no-skin');
             $('body').attr('class', 'login-layout light-login');
         },
+		data: function() {
+			return {
+				user: {},
+			}
+		},
         methods: {
             login(){
-                this.$router.push("/welcome");
+				let _this = this;
+				_this.user.password = hex_md5(_this.user.password + KEY);
+				Loading.show();
+				_this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
+					Loading.hide();
+					let resp = response.data;
+					if (resp.success) {
+						console.log(resp.content);
+						_this.$router.push("/welcome")
+					} else {
+						toast.warning(resp.message)
+					}
+				});
             }
         }
     }
