d70fa54fffb69eb8b7bd2c2b42d311807c9893d5
zuobiao <zuobiao-x@foxmail.com>
2020-12-30 19:56:41 +0800

    网站首页开发：the-course 组件增加课程等级显示

--- web/src/components/the-course.vue
+++ web/src/components/the-course.vue
@@ -11,7 +11,7 @@
                 <div class="text-muted">
                     <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{course.price}}</span>&nbsp;
                     <span class="badge badge-info"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
-<!--                    <span class="badge badge-info">{{COURSE_LEVEL | optionKV(course.level)}}</span>-->
+                    <span class="badge badge-info">{{COURSE_LEVEL | optionKV(course.level)}}</span>
                 </div>
             </div>
         </div>
@@ -26,7 +26,7 @@
         },
         data: function () {
             return {
-                // COURSE_LEVEL: COURSE_LEVEL,
+                COURSE_LEVEL: COURSE_LEVEL,
             }
         },
     }
