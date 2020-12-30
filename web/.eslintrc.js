6ffdd6effaa7fa0b4d284b35538a936637f8d022
zuobiao <zuobiao-x@foxmail.com>
2020-12-30 19:54:36 +0800

    1. 课程列表页面开发，加载所有分类 2. 增加axios接口访问日志拦截器 3. 增加.eslintrc.js 文件

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -8,13 +8,23 @@ module.exports = {
         'eslint:recommended'
     ],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console': 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-undef': 'off',
         'vue/no-unused-vars': 'off',
         'vue/require-v-for-key': 'off',
         'no-unused-vars': 'off',
-        'vue/no-unused-components': 'off'
+        'vue/no-unused-components': 'off',
+        //关闭禁止混用tab和空格
+        "no-mixed-spaces-and-tabs": [0],
+        "no-unused-vars": [2, {
+            // 允许声明未使用变量
+            "vars": "local",
+            // 参数不检查
+            "args": "none"
+        }],
+        // 关闭语句强制分号结尾
+        "semi": [0],
     },
     parserOptions: {
         parser: 'babel-eslint'
