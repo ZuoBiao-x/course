fa4772370c56f425986af0e3a9dc4686ba649c85
zuobiao <zuobiao-x@foxmail.com>
2020-12-31 11:43:46 +0800

    1. 会员登录注册功能开发，登录框显示验证码

--- web/src/components/login.vue
+++ web/src/components/login.vue
@@ -28,7 +28,7 @@
                         <div class="form-group">
                             <div class="checkbox">
                                 <label>
-                                    <input type="checkbox" class=" rememberMe" v-model="rememberMe"> 记住密码
+                                    <input type="checkbox" class="remember" v-model="remember"> 记住密码
                                 </label>
                                 <div class="pull-right">
                                     <a href="javascript:;" v-on:click="toForgetDiv()">忘记密码</a>&nbsp;
@@ -134,7 +134,7 @@
                 memberForget: {},
                 memberRegister: {},
 
-                rememberMe: true, // 记住密码
+                remember: true, // 记住密码
                 imageCodeToken: ""
             }
         },
@@ -148,7 +148,9 @@
              * 打开登录注册窗口
              */
             openLoginModal() {
-                // let _this = this;
+                let _this = this;
+                // 显示登录框时就刷新一次验证码图片
+                _this.loadImageCode();
                 $("#login-modal").modal("show");
             },
 
@@ -181,6 +183,16 @@
                 })
             },
 
+            //---------------登录框-----------------
+            /**
+             * 加载图形验证码
+             */
+            loadImageCode: function () {
+                let _this = this;
+                _this.imageCodeToken = Tool.uuid(8);
+                $('#image-code').attr('src', process.env.VUE_APP_SERVER + '/business/web/kaptcha/image-code/' + _this.imageCodeToken);
+            },
+
         }
     }
 </script>
@@ -201,7 +213,7 @@
         max-width: 400px;
     }
 
-    #login-modal input:not(.rememberMe) {
+    #login-modal input:not(.remember) {
         height: 45px;
         font-size: 16px;
     }
